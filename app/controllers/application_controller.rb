cd30dc598ae586c2b9f3cf84626f476d28512f06
Mathieu <mathieu.abeille@gmail.com>
2021-01-10 15:25:56 +0100

    fr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :formbox
+
+  def formbox
+    @lead = Lead.new
+  end
 end
